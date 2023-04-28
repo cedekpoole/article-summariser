21e9e64a2e3b93412a2fe1328f5a8f027d3a82b5
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-04-28 18:34:42 +0100

    install react spinner + add form input for URL in demo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import Title from "./assets/title.png";
+
 import Hero from "./components/Hero";
 import Demo from "./components/Demo";
 
 const App = () => {
   return (
     <main>
-      <div>
-        <div className="bg-gradient" />
-      </div>
       <div className="app font-space">
         <Hero />
         <Demo />
--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { MdOutlineContentCopy } from "react-icons/md";
+import { BsLink45Deg } from "react-icons/bs";
+import { TiTick } from "react-icons/ti";
+import { Dna } from "react-loader-spinner";
 
 const Demo = () => {
-  return (
-    <div>Demo</div>
-  )
-}
+  const [ article, setArticle ] = useState({
+    url: '',
+    summary: '',
+  }) 
 
-export default Demo
+  const handleSubmit = async (e) => {
+    alert("submitted")
+  }
+
+  return (
+    <section className="mt-10  w-5/6 mx-auto max-w-lg">
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <BsLink45Deg className="absolute left-0 ml-3 w-5" />
+          <input
+          className="block w-full rounded-md border border-gray-200 bg-white py-2.5 pl-10 text-sm shadow-lg focus:border-black focus:outline-none focus:ring-0"
+            type="url"
+            placeholder="Enter your URL"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+          />
+          <button
+          className="hover:border-gray-700 hover:text-gray-700 absolute inset-y-0 right-0 my-1.5 mr-1.5 flex w-10 items-center justify-center rounded border border-gray-200 font-sans text-sm font-medium text-gray-400 "
+          type="submit">
+
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Demo;
